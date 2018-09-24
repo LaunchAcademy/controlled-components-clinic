461e97c4ecb8ef2d040de46369416220bbfbac46
Nick Alberts <nwalberts5@gmail.com>
2018-09-24 12:25:27 -0400

    create base version of app

--- react/src/containers/App.js
+++ react/src/containers/App.js
@@ -1,71 +1,29 @@
 import React, { Component } from 'react';
 
-import TaskList from '../components/task_list'
-import TaskForm from '../components/task_form';
-
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       tasks: []
+      // This will eventually be an array of objects e.g. {taskName: sing cottlestone pie}
     }
-    this.handleDescriptionChange = this.handleDescriptionChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   componentDidMount(){
-    let that = this
-    fetch('/api/v1/tasks')
-    .then(response => {
-      return response.json()
-    })
-    .then(json => {
-      if(json.tasks){
-        this.setState({ tasks: json.tasks })
-      }
-    })
+    // For if we wish to do some fetching.
+    // fetch('/SOME-ENDPOINT')
+    // .then(response => {
+    //   return response.json()
+    // })
+    // .then(json => {
+    //   ...
+    // })
   }
 
-  // handleClearForm(event){
-  //   event.preventDefault()
-  //   this.setState({
-  //     taskDescription: '',
-  //     submissionError: ''
-  //   })
-  // }
-
-  handleSubmit(event) {
-    event.preventDefault()
-    if(!this.state.taskDescription) {
-      this.setState({
-        submissionError: 'Please specify a description'
-      })
-    }
-    else {
-      // fetch('/api/v1/tasks', { method: 'post', body:
-      //   JSON.stringify({task: {description: this.state.taskDescription}})})
-      // this.setState({
-      //   submissionError: '',
-      //   taskDescription: ''
-      // })
-
-    }
-  }
-
-  handleDescriptionChange(event) {
-    event.preventDefault()
-    this.setState({
-      taskDescription: e.target.value
-    })
-  }
   render() {
     return (
       <div>
-        <TaskList tasks={this.state.tasks } />
-        <TaskForm taskDescription={this.state.taskDescription }
-          submissionError={ this.state.submissionError }
-          handleSubmit={ this.handleSubmit }
-          handleDescriptionChange={ this.handleDescriptionChange } />
+        <h1>Pooh Tasks List</h1>
       </div>
     )
   }
